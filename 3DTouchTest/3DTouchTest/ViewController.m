ffad424bbc265b52e02f607006ccf6968ce7c33d
Mr_Yu <714362120@qq.com>
2016-09-13 14:16:44 +0800

    3DTouch
    Practice for 3DTouch

---
+++ 3DTouchTest/3DTouchTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  3DTouchTest
+//
+//  Created by wapage-mac on 16/9/12.
+//  Copyright © 2016年 wapage-mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"3DTouch";
+    self.view.backgroundColor = [UIColor grayColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
